83af982a38302355c327caacc66d87e581bd4766
Kennedy Miranda <kennedymirandavenancio8@gmail.com>
2025-12-07 21:11:04 +0000

    update end delete

--- casa-pet/CasaPet.Api/obj/Debug/net8.0/CasaPet.Api.AssemblyInfo.cs
+++ casa-pet/CasaPet.Api/obj/Debug/net8.0/CasaPet.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaPet.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853df1fab10f2571395eb9974a4889dc19370352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17787013c6d6ff78773cefa16c4ea62bd7ac2104")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaPet.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaPet.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
